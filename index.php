3876f0e5a6e71e7fad502dca47d50ef537507be8
awaist7860 <48568803+awaist7860@users.noreply.github.com>
2021-01-14 18:21:00 +0000

    Awais fixed index.php
    accidentally deleted php tags

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-
+<?php
 session_start();                                                            //Starts session
 $view = new stdClass();                                                     //New class
 $view->pageTitle = 'Homepage';                                              //Giving a page title which is homepage
