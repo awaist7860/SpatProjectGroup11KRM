5370c45380b69eb679159320f874e0a0c16ba2ec
danyal9000 <74073994+danyal9000@users.noreply.github.com>
2021-01-14 13:55:43 +0000

    Danyal updated front end
    Updated the front end

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 <?php
+session_start();                                                            //Starts session
+$view = new stdClass();                                                     //New class
+$view->pageTitle = 'Homepage';                                              //Giving a page title which is homepage
+require_once('Views/index.phtml');                                          //Requiring the index page
